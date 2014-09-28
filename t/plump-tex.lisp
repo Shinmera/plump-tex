bac870845f6eb0fbd3801cdaa78c21541d87590e
Fernando Borretti <eudoxiahp@gmail.com>
2014-09-28 19:13:31 -0300

    Add more tests

--- t/plump-tex.lisp
+++ t/plump-tex.lisp
@@ -16,6 +16,11 @@
   (is (equal (parse-print "test") "test"))
   (is (equal (parse-print " \\test") " <test/>"))
   (is (equal (parse-print " {}") " <div/>"))
-  (is (equal (parse-print " {1}") " <div>1</div>")))
+  (is (equal (parse-print " {1}") " <div>1</div>"))
+  (is (equal (parse-print " {{}}") " <div><div/></div>"))
+  (is (equal (parse-print " \\test{1 2 3}") " <test>1 2 3</test>"))
+  (is (equal (parse-print " \\a{\\b{\\c{}}}") " <a><b><c/></b></a>"))
+  (is (equal (parse-print " \\a{\\b{\\c1{123}\\c2{123}}}")
+             " <a><b><c1>123</c1><c2>123</c2></b></a>")))
 
 (run! 'tests)
